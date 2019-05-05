0087d2e0b3e5cac376e5315e1367508b8130e7cd
xianchangyu <ol939965761@qq.com>
2019-05-05 17:15:39 +0800

    merge

--- build/my-first-plugin.js
+++ build/my-first-plugin.js
@@ -12,30 +12,25 @@ const {
   AsyncSeriesBailHook,
   AsyncSeriesWaterfallHook
 } = require('tapable')
+const path = require('path')
+const fs = require('fs')
 
 class MyFirstPlugin {
   constructor(option = {}) {
     this.option = option
-    console.log('MyFirstPlugin option:', this.option)
   }
 
   // 将 `apply` 定义为其原型方法，此方法以 compiler 作为参数
   apply(compiler) {
     // 指定要附加到的事件钩子函数
-    console.log('MyFirstPlugin compiler:', compiler)
-    // 指定要附加到的事件钩子函数
-    // compiler.hooks.emit.tapAsync('MyExampleWebpackPlugin', (compilation, callback) => {
-    //   console.log('This is an example plugin!')
-    //   console.log(
-    //     'Here’s the `compilation` object which represents a single build of assets:',
-    //     compilation
-    //   )
+    // console.log('MyFirstPlugin compiler:', compiler)
+    const assetsRoot = path.resolve(__dirname, '../dist-' + process.env.env_config)
+    const logger = fs.createWriteStream('log.txt', {
+      flags: 'a' // 'a' means appending (old data will be preserved)
+    })
 
-    //   // 使用 webpack 提供的 plugin API 操作构建结果
-    //   compilation.addModule(/* ... */)
-
-    //   callback()
-    // })
+    logger.write(assetsRoot)
+    logger.end()
   }
 }
 
