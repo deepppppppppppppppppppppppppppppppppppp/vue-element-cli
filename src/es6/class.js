3b73637c285306ac59935979a1d6116750c34a96
xianchangyu <ol939965761@qq.com>
2019-05-09 16:13:18 +0800

    1

--- src/es6/class.js
+++ src/es6/class.js
@@ -5,10 +5,10 @@ class Parent {
     this.sex = sex
   }
   static sayHello() {
-    console.log('hello')
+    // console.log('hello')
   }
   sayName() {
-    console.log('my name is ' + this.name)
+    // console.log('my name is ' + this.name)
     return this.name
   }
 }
@@ -18,45 +18,45 @@ class Child extends Parent {
     this.age = age
   }
   sayAge() {
-    console.log('my age is ' + this.age)
+    // console.log('my age is ' + this.age)
     return this.age
   }
 }
 let parent = new Parent('Parent', 'man')
 let child = new Child('Child', 18, 'manman')
-console.log('parentsex:', parent.sex)
-console.log('childsex:', child.sex)
-console.log('parent: ', parent) // parent:  Parent {name: "Parent"}
+// console.log('parentsex:', parent.sex)
+// console.log('childsex:', child.sex)
+// console.log('parent: ', parent) // parent:  Parent {name: "Parent"}
 Parent.sayHello() // hello
 parent.sayName() // my name is Parent
-console.log('child: ', child) // child:  Child {name: "Child", age: 18}
+// console.log('child: ', child) // child:  Child {name: "Child", age: 18}
 Child.sayHello() // hello
 child.sayName() // my name is Child
 child.sayAge() // my age is 18
 let a = new Map()
 a.set('name', 'xcy')
-console.log(a, a.get('name'))
-console.log(a, a.has('name'))
+// console.log(a, a.get('name'))
+// console.log(a, a.has('name'))
 
 let b = new Set()
-console.log(b)
+// console.log(b)
 b.add('age')
-console.log(b, b.has('age'), b.size)
-console.log(Array.from(b))
-console.log([...b])
+// console.log(b, b.has('age'), b.size)
+// console.log(Array.from(b))
+// console.log([...b])
 
 function Nn() {
   this.name = 1
 }
 Nn.prototype.age = function() {
-  console.log(this.name)
+  // console.log(this.name)
 }
 
 function Nnn() {
   Nn.call(this)
 }
 var nn = new Nnn()
-console.log(nn)
+// console.log(nn)
 export default {
   Parent,
   Child
